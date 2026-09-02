ad9e0e0caedb0a41669635ff46f82a9456f68542
KaueElias011 <kaueelias.011@gmail.com>
2026-09-01 22:07:23 -0300

    Algumas Linguagens en Java
    MarkDOwn

---
+++ HelloWorld.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello world");
+    System.out.println("Materia Nova");
+    System.out.println(3*3);
+
+// Entrada De Valores    
+    int Numero1 = 10;
+    int Numero2 = 20;
+    int resultado;   
+ 
+    
+    // Processamento
+    resultado = Numero1 + Numero2;
+    System.out.println("O Resultado Da Soma é: " + resultado ) ;
+    resultado = Numero1 / Numero2;    
+    System.out.println("O Resultado Da Divisão é: " + resultado );
+    resultado = Numero1 * Numero2;
+    System.out.println("O Resultado Da Multiplicação é: " + resultado );
+    resultado = Numero1 - Numero2;
+    System.out.println("O Resultado Da Subtração é: " + resultado );
+    
+    // Saida De Valores 
+    
+    System.out.println(resultado);
+    
+   
+ 
+
+// TODO code application logic here
+    }
+    
+}
